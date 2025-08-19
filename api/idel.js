944dcf40f09c5e36679766ffb8e2c8d38f530d8f
Guillaume Guimond <guillaume.guimond@orange.com>
2025-08-19 15:13:53 +0200

    securing cron job

--- api/idel.js
+++ api/idel.js
@@ -3,6 +3,12 @@ const run = require('../idel.js')
 module.exports.maxDuration = 60;
 
 module.exports.GET = async function GET(request) {
+  const authHeader = request.headers.get('authorization');
+  if (authHeader !== `Bearer ${process.env.CRON_SECRET}`) {
+    return new Response('Unauthorized', {
+      status: 401,
+    });
+  }
   await run()
   return new Response('OK')
 }
